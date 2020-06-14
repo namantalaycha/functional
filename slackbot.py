bd36c4375076ea09aef5fc28343d854b0a7f0660
namantalaycha <naman.talaycha@myntra.com>
2020-06-14 19:27:20 +0530

    new files

--- slackbot.py
+++ slackbot.py
@@ -1,5 +1,7 @@
 import sys
 from slacker import Slacker
-slack = Slacker('xoxb-1194530660337-1206300796208-gpwoVts9WPOYXkq5zqoNtGtZ')
+slack = Slacker('xoxb-1194530660337-1206300796208-XzURRjkJB5a1caEXUqfT13ui')
 message="hello everyone"
+
 slack.chat.post_message('#demo' , message );
+#` go test ./middleware/ | grep o -m 1| awk '{ print $1}'`
