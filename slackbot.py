6f9c4fcba97c4f194dc54b371cc53724f30cddfd
namantalaycha <naman.talaycha@myntra.com>
2020-06-19 03:59:45 +0530

    delete files

--- slackbot.py
+++ slackbot.py
@@ -1,6 +1,6 @@
 import sys
 from slacker import Slacker
-slack = Slacker('xoxb-1194530660337-1206300796208-N1jzcyd4zK2fPGQchs55VGw7')
+slack = Slacker('xoxb-1194530660337-1206300796208-czaoeUCtyLBLc3Idm0MGU0A2')
 f = open("temp/text.txt", "r")
 message= f.read()
 
