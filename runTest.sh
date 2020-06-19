6f3f718e341292f9970ca125a5dba099a580632f
namantalaycha <naman.talaycha@myntra.com>
2020-06-19 15:36:18 +0530

    changes made

--- runTest.sh
+++ runTest.sh
@@ -2,10 +2,10 @@
 
 touch temp/text.txt
 echo "result of Testing:" > temp/text.txt
-go test ./middleware >> temp/text.txt
+go test -cover ./middleware >> temp/text.txt
 echo "result of Benchmarks :" >> temp/text.txt
 go test -bench=. ./middleware >> temp/text.txt
 echo "result of Functional Testing:" >> temp/text.txt
-go test ./test>> temp/text.txt
+go test -cover ./test>> temp/text.txt
 echo "result of Functional Benchmarks :" >> temp/text.txt
 go test -bench=. ./test >> temp/text.txt
