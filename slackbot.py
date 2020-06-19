f35a9b24151598e4a27a88482eaed42d25d7f061
namantalaycha <naman.talaycha@myntra.com>
2020-06-19 13:02:33 +0530

    msg

--- slackbot.py
+++ slackbot.py
@@ -1,6 +1,6 @@
 import sys
 from slacker import Slacker
-slack = Slacker('xoxb-1194530660337-1206300796208-czaoeUCtyLBLc3Idm0MGU0A2')
+slack = Slacker('xoxb-1194530660337-1206300796208-To4noH0m82VZaauXLoLTixuy')
 f = open("temp/text.txt", "r")
 message= f.read()
 
